325489a8e5712fb46f5be3ad550644832eb18fb4
Lucky <siddharth.luckyreddy@gmail.com>
2020-05-28 19:11:05 -0400

    - Also added the Enable JPA tags.

--- department-info-service/src/main/java/lucky/restapi/departmentinfoservice/DepartmentInfoServiceApplication.java
+++ department-info-service/src/main/java/lucky/restapi/departmentinfoservice/DepartmentInfoServiceApplication.java
@@ -2,7 +2,9 @@ package lucky.restapi.departmentinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "lucky.restapi.departmentinfoservice.repository")
 @SpringBootApplication
 public class DepartmentInfoServiceApplication {
 
