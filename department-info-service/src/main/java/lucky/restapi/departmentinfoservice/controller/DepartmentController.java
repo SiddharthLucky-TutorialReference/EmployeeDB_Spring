e7d3f115abe00d9c251bba269949427cb6823e19
Lucky <siddharth.luckyreddy@gmail.com>
2020-06-04 12:36:38 -0400

    - Updated the spring project, updated the DepartmentController and Departments, removed and updated the necessary annotations

--- department-info-service/src/main/java/lucky/restapi/departmentinfoservice/controller/DepartmentController.java
+++ department-info-service/src/main/java/lucky/restapi/departmentinfoservice/controller/DepartmentController.java
@@ -4,27 +4,35 @@ import lucky.restapi.departmentinfoservice.model.Departments;
 import lucky.restapi.departmentinfoservice.repository.DepartmentInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.*;
 
-@Controller
 @RestController
 @RequestMapping("/departments")
 public class DepartmentController
 {
-    private final DepartmentInterface departmentInterface;
-
     @Autowired
-    public DepartmentController(DepartmentInterface departmentInterface) {
-        this.departmentInterface = departmentInterface;
-    }
+    DepartmentInterface departmentInterface;
 
     @GetMapping("/all")
     public List<Departments> getAllDepartments()
     {
-        List<Departments> allDepartments = (List<Departments>) departmentInterface.findAll();
-        return allDepartments;
+        return departmentInterface.findAll();
+    }
+
+    @PostMapping(value = "/add-departments")
+    public String addIntoDepartments(@RequestBody Departments department)
+    {
+        departmentInterface.save(department);
+        return "Value added Successfully";
+    }
+
+
+    @DeleteMapping(value ="/remove-department/{dept_id}")
+    public String removeDepartmentId(@PathVariable("dept_id") String dept_Id)
+    {
+        departmentInterface.deleteById(dept_Id);
+        return "The value is deleted successfully";
     }
 }
